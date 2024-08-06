31dfffa9cb0a6dc40ace215177d498d1207c6b49
unknown <delacruzhernandezoscardavid@outlook.com>
2024-08-06 08:06:17 -0600

    tablas 3

--- app/views/content/bibliotecario/admSubcategoria-vista.php
+++ app/views/content/bibliotecario/admSubcategoria-vista.php
@@ -13,13 +13,12 @@
     </thead>
     <tbody>
       <?php if (isset($Consulta)) {
-          foreach ($Consulta as $autor) {
-              echo '<form class="form" action="/bibliotecaMvc/index?clase=controladorAutor&metodo=EliminaActualizaAutor" method="POST">';
+          foreach ($Consulta as $sub) {
+              echo '<form class="form" action="/bibliotecaMvc/index?clase=controladorSubcategoria&metodo=EliminaActualizaSubcategoria" method="POST">';
               echo '<tr>';
-              echo '<td> <input type="text" name="txtidautor" value="' . $autor['idautor'] . '" readonly> </td>';
-              echo '<td> <input type="text" name="txtNombre" value="' . $autor['vchNombre'] . '"> </td>';
-              echo '<td> <input type="text" name="txtAp" value="' . $autor['vchAp'] . '"> </td>';
-              echo '<td> <input type="text" name="txtAm" value="' . $autor['vchAm'] . '"> </td>';
+              echo '<td> <input type="text" name="txtidsub" value="' . $sub['idsubcategoria'] . '" readonly> </td>';
+              echo '<td> <input type="text" name="txtsubcategoria" value="' . $sub['vchSubcategoria'] . '"> </td>';
+              echo '<td> <input type="text" name="txtidcategoria" value="' . $sub['idcategoria'] . '"> </td>';
               echo '<td width=250>';
               echo '<button type="submit" name="btnEliminar" value="btnEliminar" class="submit-button">Eliminar</button>';
               echo '&nbsp;';
